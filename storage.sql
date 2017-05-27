28a7d83475340325999ef2dccffe5afbcf5aec00
Witkowp <witkowskipiotr96@gmail.com>
2017-05-27 16:23:01 +0200

    Add files via upload

--- storage.sql
+++ storage.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 27 Maj 2017, 15:15
+-- Czas generowania: 27 Maj 2017, 16:22
 -- Wersja serwera: 10.1.19-MariaDB
 -- Wersja PHP: 5.6.28
 
@@ -57,16 +57,17 @@ CREATE TABLE `studenci` (
   `email` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `nr_indeksu` int(11) NOT NULL,
   `wydzial` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
-  `kierunek` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+  `kierunek` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `semestr` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Zrzut danych tabeli `studenci`
 --
 
-INSERT INTO `studenci` (`idstudenta`, `imie`, `nazwisko`, `haslo`, `email`, `nr_indeksu`, `wydzial`, `kierunek`) VALUES
-(1, 'Jan', 'Kowalski', ' 040b7cf4a55014e185813e0644502ea9', 'jak.kowalski@gmail.com', 256194, 'Mechatronika', 'Maechatronika'),
-(2, 'Mateusz', 'Nowak', '9be00b007ef836db60e642b9e812ab12', 'mateusz.nowak@gmail.com', 280543, 'Elektryczny', 'Informatyka');
+INSERT INTO `studenci` (`idstudenta`, `imie`, `nazwisko`, `haslo`, `email`, `nr_indeksu`, `wydzial`, `kierunek`, `semestr`) VALUES
+(1, 'Jan', 'Kowalski', ' 040b7cf4a55014e185813e0644502ea9', 'jak.kowalski@gmail.com', 256194, 'Mechatronika', 'Maechatronika', 2),
+(2, 'Mateusz', 'Nowak', '9be00b007ef836db60e642b9e812ab12', 'mateusz.nowak@gmail.com', 280543, 'Elektryczny', 'Informatyka', 1);
 
 -- --------------------------------------------------------
 
