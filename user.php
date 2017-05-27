8dce07db970941b73cc589553f961ee79e5b2f81
marnow955 <marnow955@gmail.com>
2017-05-27 15:47:48 +0200

    add user.php main.css(user)

---
+++ user.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+ <html lang="pl">
+   <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+     <title>Student Storage</title>
+     <meta name="description" content="Student storage" />
+     <meta name="keywords" content="student, storage" />
+
+     <link rel="stylesheet" href="css/main.css" type="text/css" />
+   </head>
+
+   <body>
+     <div class="menubar">
+       <div class="mbutton" id="upload">Upload</div>
+       <div class="mbutton" id="wyloguj">Wyloguj</div>
+       <div class="mbutton">Ustawienia</div>
+     </div>
+     <div class="lista">
+       <div class="przedmiot">Matematyka</div>
+       <div class="przedmiot">Materiałoznawstwo</div>
+       <div class="przedmiot">Informatyk</div>
+       <div class="przedmiot">Fizyka</div>
+       <div class="przedmiot">Języki i metody programowania</div>
+       <div class="przedmiot">Podstawy Intensywnego Opierdalania</div>
+     </div>
+   </body>
+  </html>
