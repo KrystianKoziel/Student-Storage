43480a3262a714149b43bc9988fb2896625832d6
KrystianKoziel <krystian.koziel02@gmail.com>
2017-05-27 16:10:21 +0200

    Create sidebar

---
+++ sidebar.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="Description" content="">
+	<meta name="Keywords" content="pliki  studia ">
+	<meta name="Author" content="Krystian Kozieł">
+	<link rel="stylesheet" href="sidebar.css" type="text/css">
+	<title>Pliki pliki pliki</title>
+</head>
+<body>
+
+<div id="content">
+	<iframe name="main" src="user.php"></iframe>
+
+		<div id="sidebar">
+
+			<div id="menu">
+				<div class="title">Lista semestrów</div>
+				<ul id="menu-content">
+					<li class="dropdown">
+						<div class="dropdown-content" onload="#">
+
+						</div>
+						<a target="main" class="dropbtn">Sem I</a>
+					</li>
+
+					<li class="dropdown">
+            <div class="dropdown-content" onload="#">
+
+						</div>
+
+						<a target="main" class="dropbtn">Sem II</a>
+
+					</li>
+					<li class="dropdown">
+
+						<div class="dropdown-content" onload="#">
+
+						</div>
+						<a target="main" class="dropbtn">Sem III</a>
+
+          </li>
+
+					<li class="dropdown">
+						<div class="dropdown-content" onload="#">
+
+						</div>
+						<a target="main" class="dropbtn">Sem IV</a>
+					</li>
+
+					<li class="dropdown">
+						<div class="dropdown-content" onload="#">
+
+						</div>
+
+						<a target="main" class="dropbtn">Sem V</a>
+					</li>
+
+					<li class="dropdown">
+            <div class="dropdown-content" onload="#">
+
+						</div>
+						<a target="main" class="dropbtn">Sem VI</a>
+					</li>
+
+          <li class="dropdown">
+						<div class="dropdown-content" onload="#">
+
+						</div>
+						<a target="main" class="dropbtn">Sem VII</a>
+					</li>
+
+
+				</ul>
+			</div>
+
+	</div>
+</div>
+</body>
