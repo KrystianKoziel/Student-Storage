41a7b4ac825fd02dd375922ffec61238bab0f885
marnow955 <marnow955@gmail.com>
2017-05-28 05:55:03 +0200

    poprawka2

--- load.js
+++ load.js
@@ -3,7 +3,7 @@ function load(id) {
          'getusersem.php',
          {
           method: 'post',
-          asynchronous: false,
+          asynchronous: true,
           parameters: "id=" + id,
           onComplete: function(response) {
             loadsem(id,response);
@@ -42,56 +42,39 @@ function loadsub(id, sem) {
           asynchronous: true,
           parameters: "id=" + id + "&" + "sem=" + sem,
           onComplete: function(response) {
-          retsub(id, sem, response) ;
+          retsub(sem, response) ;
         },
           onFailure: showAlert
        });
 }
-function retsub(id, sem, response){
+function retsub(sem, response){
   var a = 's'+sem;
   var lista = document.getElementById(a);
   var wyniki = response.responseText.split("\n");
   lista.innerHTML='';
   for(i=0; i < wyniki.length; i++){
-     var przedmiot = '<a target="main" href="przedmiot.php" onclick="loadPliki('+id+', '+sem+ ','  +wyniki[i] + ')" >' + wyniki[i] +'</a> ';
+     var przedmiot = '<a target="main" href="#">'+ wyniki[i] +'</a> ';
      lista.innerHTML += przedmiot;
    }
 }
-function loadPliki(id, sem, przedmiot) {
+var semestrres = 0;
+function getSemestr(id) {
   var myAjax = new Ajax.Request(
-         'loadplik.php',
+         'getusersem.php',
          {
           method: 'post',
-          asynchronous: true,
-          parameters: "id=" + id + "&" + "sem=" + sem,
+          asynchronous: false,
+          parameters: "id=" + id,
           onComplete: function(response) {
-          retPlik( sem, response, przedmiot) ;
-        },
+            semestrres = response.responseText;
+          },
           onFailure: showAlert
        });
 }
-function retPlik(sem, response, przedmiot) {
-      var s = response.responseText.split("\n");
-      var a ='pliki/'+ s[0]+'/'+s[1]+'/'+sem+'/'+przedmiot;
-      var table = document.getElementById("idlista");
-      var string = "<table><td><tr>Nazwa pliku</tr><tr>Rozmiar</tr><tr>Typ</tr></td>";
-      string += `
-      <?php
-	     $dir = "`;
-       string += a;
-       string += ` $files = scandir($dir);
-	       foreach ($files as $file)
-	        {
-		          if (!in_array($file,array(".","..")))
-		            {
-			               $size = filesize($dir.'/'.$file);
-			                  $size = round($size / 1024);
-			                     $exten = pathinfo($file);
-			                        echo "<tr><td><a href='download.php?file=".$file."'>".$exten['filename']."</a></td><td>".$size."KB</td><td>".$exten['extension']."</td>	</tr>"";
-		            }
-	        }
-      ?>
-      `;
-      string += "</table>";
-      table.innerHTML = string;
+function getsemres() {
+  return semestrres;
+}
+function upload(){
+		var input = document.getElementById("filesToUpload");
+		input.click();
 }
--- loadplik.php
+++ loadplik.php
@@ -1,25 +1,25 @@
 <?php
-   require_once "connect.php";
+    require_once "connect.php";
 
-   if($polaczenie->connect_errno!=0)
-   {
-     echo "Error: ".$polaczenie->connect_errno;
-   }
-   else
-   {
-     $id = $_POST['id'];
-     $sem = $_POST['sem'];
-     $zapytanie = "SELECT wydzial FROM studenci WHERE idstudenta=$id";
-     $wynik = $polaczenie->query($zapytanie);
-     $temp = $wynik->fetch_assoc();
-     $wydzial = strtolower($temp['wydzial']);
-     $str .= $wydzial."\n";
-     $zapytanie = "SELECT kierunek FROM studenci WHERE idstudenta=$id";
-     $wynik = $polaczenie->query($zapytanie);
-     $temp = $wynik->fetch_assoc();
-     $kierunek = $temp['kierunek'];
-     $str .= $kierunek."\n";
-    
-     echo rtrim($str);
-   }
-  ?>
+    if($polaczenie->connect_errno!=0)
+    {
+      echo "Error: ".$polaczenie->connect_errno;
+    }
+    else
+    {
+      $id = $_POST['id'];
+      $sem = $_POST['sem'];
+      $zapytanie = "SELECT wydzial FROM studenci WHERE idstudenta=$id";
+      $wynik = $polaczenie->query($zapytanie);
+      $temp = $wynik->fetch_assoc();
+      $wydzial = strtolower($temp['wydzial']);
+      $str .= $wydzial."\n";
+      $zapytanie = "SELECT kierunek FROM studenci WHERE idstudenta=$id";
+      $wynik = $polaczenie->query($zapytanie);
+      $temp = $wynik->fetch_assoc();
+      $kierunek = $temp['kierunek'];
+      $str .= $kierunek."\n";
+
+      echo rtrim($str);
+    }
+?>
