148ae3e0946c80b8a49752604d5d4f483fded1bd
KrystianKoziel <krystian.koziel02@gmail.com>
2017-05-28 05:30:05 +0200

    commit jeszcze jeden loady porobione

--- load.js
+++ load.js
@@ -3,7 +3,7 @@ function load(id) {
          'getusersem.php',
          {
           method: 'post',
-          asynchronous: true,
+          asynchronous: false,
           parameters: "id=" + id,
           onComplete: function(response) {
             loadsem(id,response);
@@ -42,39 +42,56 @@ function loadsub(id, sem) {
           asynchronous: true,
           parameters: "id=" + id + "&" + "sem=" + sem,
           onComplete: function(response) {
-          retsub(sem, response) ;
+          retsub(id, sem, response) ;
         },
           onFailure: showAlert
        });
 }
-function retsub(sem, response){
+function retsub(id, sem, response){
   var a = 's'+sem;
   var lista = document.getElementById(a);
   var wyniki = response.responseText.split("\n");
   lista.innerHTML='';
   for(i=0; i < wyniki.length; i++){
-     var przedmiot = '<a target="main" href="#">'+ wyniki[i] +'</a> ';
+     var przedmiot = '<a target="main" href="przedmiot.php" onclick="loadPliki('+id+', '+sem+ ','  +wyniki[i] + ')" >' + wyniki[i] +'</a> ';
      lista.innerHTML += przedmiot;
    }
 }
-var semestrres = 0;
-function getSemestr(id) {
+function loadPliki(id, sem, przedmiot) {
   var myAjax = new Ajax.Request(
-         'getusersem.php',
+         'loadplik.php',
          {
           method: 'post',
-          asynchronous: false,
-          parameters: "id=" + id,
+          asynchronous: true,
+          parameters: "id=" + id + "&" + "sem=" + sem,
           onComplete: function(response) {
-            semestrres = response.responseText;
-          },
+          retPlik( sem, response, przedmiot) ;
+        },
           onFailure: showAlert
        });
 }
-function getsemres() {
-  return semestrres;
-}
-function upload(){
-		var input = document.getElementById("filesToUpload");
-		input.click();
+function retPlik(sem, response, przedmiot) {
+      var s = response.responseText.split("\n");
+      var a ='pliki/'+ s[0]+'/'+s[1]+'/'+sem+'/'+przedmiot;
+      var table = document.getElementById("idlista");
+      var string = "<table><td><tr>Nazwa pliku</tr><tr>Rozmiar</tr><tr>Typ</tr></td>";
+      string += `
+      <?php
+	     $dir = "`;
+       string += a;
+       string += ` $files = scandir($dir);
+	       foreach ($files as $file)
+	        {
+		          if (!in_array($file,array(".","..")))
+		            {
+			               $size = filesize($dir.'/'.$file);
+			                  $size = round($size / 1024);
+			                     $exten = pathinfo($file);
+			                        echo "<tr><td><a href='download.php?file=".$file."'>".$exten['filename']."</a></td><td>".$size."KB</td><td>".$exten['extension']."</td>	</tr>"";
+		            }
+	        }
+      ?>
+      `;
+      string += "</table>";
+      table.innerHTML = string;
 }
